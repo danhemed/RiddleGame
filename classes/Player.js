515265250e864ef3146d1767890b2d7e71be070c
Dan <danhemed123@gmail.com>
2025-06-26 15:18:47 +0300

    bulid the classes player and riddle, and add a file in folder player 'playerName'

--- classes/Player.js
+++ classes/Player.js
@@ -1,4 +1,20 @@
-import readline from 'readline-sync';
+export default class Player {
+    constructor(name) {
+        this.name = name;
+        this.times = [];
+    }
 
-const playerName = readline.question("What is your name?");
-console.log("Hello " + playerName);
+    recordTime(start, end) {
+        const seconds = (end - start) / 1000;
+        this.times.push(seconds);
+    }
+
+    showStats() {
+        const totalTime = this.times.reduce((sum, i) => sum + i, 0);
+        const averageTime = totalTime / this.times.length;
+
+        console.log(`This ${this.name}'s stats: `);
+        console.log("Total Time: " + totalTime);
+        console.log("Average Time: " + averageTime);
+    }
+}
