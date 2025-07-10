5dd1f360ae03c683612defbe006443177b3c258d
Dan <danhemed123@gmail.com>
2025-07-10 11:40:33 +0300

    finish menus

--- Client/app.js
+++ Client/app.js
@@ -1,6 +1,31 @@
 import { CRUD } from "../Server/services/generic.crud.js";
+import { Menu, MenuRiddle, MenuPlayer, MenuDelete } from "./menus.js";
 
 const player = new CRUD("../Server/db/players.txt");
-player.Create({id: 1, name: "dan"});
+const riddle = new CRUD("../Server/db/riddles.txt");
+const leader = new CRUD("../Server/db/riddles.txt");
 
-player.GetAll().then(p => console.log(p));
+
+do {
+    switch (Menu()) {
+        case '1':
+            break;
+        case '2':
+            await riddle.Create(MenuRiddle());
+            break;
+        case '3':
+            console.log(await riddle.GetAll());
+            break;
+        case '4':
+            await riddle.Update(MenuRiddle().id, MenuRiddle())
+            break;
+        case '5':
+            await riddle.Delete(MenuDelete());
+            break;
+        case '6':
+            break;
+        default:
+            console.log(`plase choose only number from 0 to 6!!`);
+            break;
+    }
+} while (choice !== '0');
