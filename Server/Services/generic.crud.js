852277cfea79fedcbe76255ef98df9169084501e
Dan <danhemed123@gmail.com>
2025-07-10 16:13:14 +0300

    finish part 2 the client

--- Server/Services/generic.crud.js
+++ Server/Services/generic.crud.js
@@ -17,7 +17,15 @@ export class CRUD {
 
     async Create(newItem) {
         const items = await this.GetAll();
-        items.push(newItem);
+
+        const exists = items.some(i => i.id == newItem.id);
+        if (!exists) {
+            items.push(newItem);
+        } else {
+            console.log(`The id already exists!`);
+            return;
+        }
+
         let dataString = "";
         try {
             dataString = JSON.stringify(items, null, 2);
@@ -25,15 +33,17 @@ export class CRUD {
             console.log(`ERROR! Create-JSON.stringify: ${err}`);
         }
         await WriteFile(this.filePath, dataString);
+        console.log(`Created item with id ${newItem.id}`);
         return newItem;
     }
 
-    async Update( id ,updateItem) {
+    async Update(updateItem) {
         const items = await this.GetAll();
-        const index = items.findIndex(item => item.id === id);
+        const index = items.findIndex(item => item.id === updateItem.id);
         if (index === -1) {
             throw new Error(`ERROR! Item isn't found!`);
         }
+        updateItem.id = items[index].id;
         items[index] = {...items[index], ...updateItem};
         let dataString = "";
         try {
